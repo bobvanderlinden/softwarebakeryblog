0b5a807150ec831e0e2976004fb29d8c6723e2ca
FrozenCow <frozencow@gmail.com>
2013-06-23 16:21:21 +0200

    Added syncing in publish-release script

--- publish-release.sh
+++ publish-release.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rsync -r target/release-build/ softwarebakery:/var/www/softwarebakery.com/
+mkdir -p target/remote
+sshfs softwarebakery.com:/var/www/softwarebakery.com/ target/remote
+diff -rq target/remote target/release-build
+fusermount -u target/remote
+echo -n "Continue? " && read cont
+[ "$cont" != "y" ] && exit 1
+rsync -r target/release-build/ softwarebakery:/var/www/softwarebakery.com/ --progress
